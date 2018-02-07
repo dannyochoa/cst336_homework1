ca91176488b5f1290005c11fe98a85ac1f7be7ba
Daniel Ochoa Aguila <dochoa-aguila@csumb.edu>
2018-02-07 23:37:17 +0000

    ready for heroku

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset = "utf-8"/>
+        <title>Waterfall Development Methodology</title>
+    </head>
+    
+    <body>
+        
+        <header>
+            <h1>Waterfall Development Methodology</h1>
+            <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+            <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+        </header>
+        
+        <nav>
+            <a href = "index.php" id = "standOut">Home</a> 
+            <a href = "pros.html">Pros</a>
+            <a href = "cons.html">Cons</a>
+            <a href = "otherModels.html">Other Models</a>
+        </nav>
+        
+        <main>
+            <div id = "left">
+                <!-- instead of <strong> i am using <b> !-->
+                <p><b>What is a development Methodology?</b><br/><br/>
+                 A development methodology helps engineers with planning, implementing, testing, and deploying 
+                 their programs. Engineers will use certain models when working as a team to improve efficiency and to keep things organized.
+                </p>
+                <br/>
+                <p><b>Overview of the Waterfall Methodology</b><br/><br/>
+               The waterfall model is considered to be the classic approach to the development lifecycle. It is also the most popular model. 
+               This methodology approaches problem-solving in a linear fashion.
+                </p>
+            </div>
+        </main>
+        
+        <div id = "right">
+            <img src = "img/waterfall.jpg" alt = "Waterfall Development Methodology">
+        </div>
+        
+        <footer>
+            <small>All credit goes to:</small>
+            <ul>
+                <li><a href = "http://www.itinfo.am/eng/software-development-methodologies/">Association of Modern Technologies Professionals</a></li>
+                <li><a href = "https://www.seguetech.com/waterfall-vs-agile-methodology/">Segue technologies</a></li>
+            </ul>
+        </footer>
+    </body>
+
+</html>
